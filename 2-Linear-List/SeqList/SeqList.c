b643dc4a1acd55e27dcee809e997ae3c2ccf1bfe
zjxjwxk <zjxjwxk@gmail.com>
2019-05-08 21:54:11 +0800

    add delete

--- 2-Linear-List/SeqList/SeqList.c
+++ 2-Linear-List/SeqList/SeqList.c
@@ -96,6 +96,19 @@ void insertElem(Sqlist &L, int x) {
 	++(L.length);
 }
 
+int deleteElem(Sqlist &L, int p, int &e) {
+	int i;
+	if (p < 0 || p > L.length - 1) {
+		return 0;
+	}
+	e = L.data[p];
+	for (i = p; i < L.length - 1; ++i) {
+		L.data[i] = L.data[i + 1];
+	}
+	--(L.length);
+	return 1;
+}
+
 void PrintList(SeqList L) {
 	printf("------输出元素------\n");
 	for (int i = 0; i < L.length; i++) {
