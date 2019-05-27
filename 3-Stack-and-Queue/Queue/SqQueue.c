1dc0845cb622e825f0c6c39a2a482961e197a8b9
zjxjwxk <zjxjwxk@gmail.com>
2019-05-27 21:54:32 +0800

    add SqQueue

--- 3-Stack-and-Queue/Queue/SqQueue.c
+++ 3-Stack-and-Queue/Queue/SqQueue.c
@@ -5,8 +5,54 @@
  * 顺序队
  */
 
+#define maxSize 10
+
 typedef struct {
 	int data[maxSize];
 	int front;
 	int rear;
 } SqQueue;
+
+void initQueue(SqQueue *qu) {
+	qu->front = qu->rear = 0;
+}
+
+int isQueueEmpty(SqQueue qu) {
+	if (qu.front == qu.rear) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+int enQueue(SqQueue *qu, int x) {
+	if ((qu->rear + 1) % maxSize == qu->front) {
+		return 0;
+	}
+	qu->rear = (qu->rear + 1) % maxSize;
+	qu->data[qu->rear] = x;
+	return 1;
+}
+
+int deQueue(SqQueue *qu, int *x) {
+	if (qu->front == qu->rear) {
+		return 0;
+	}
+	qu->front = (qu->front + 1) % maxSize;
+	*x = qu->data[qu->front];
+	return 1;
+}
+
+int main() {
+	SqQueue qu;
+	initQueue(&qu);
+	printf("队列是否为空: %d\n", isQueueEmpty(qu));
+	enQueue(&qu, 1);
+	enQueue(&qu, 2);
+	enQueue(&qu, 3);
+	enQueue(&qu, 4);
+	int x;
+	deQueue(&qu, &x);
+	printf("出队元素: %d\n", x);
+	return 0;
+}
