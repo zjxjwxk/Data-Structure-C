b16e70b7cb5ef3a36b2ebe9518538ef9740347f0
zjxjwxk <zjxjwxk@gmail.com>
2019-06-28 22:52:09 +0800

    add Tree

---
+++ 6-Tree/BinaryTree.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 二叉树
+ */
+
+typedef struct BTNode {
+	char data;
+	struct BTNode *lchild;
+	struct BTNode *rchild;
+} BTNode;
+
+int main() {
+	
+	return 0;
+}
