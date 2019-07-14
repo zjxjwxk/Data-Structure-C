a1dc7e94da1673f621008d78485dadb30ac93867
zjxjwxk <zjxjwxk@gmail.com>
2019-07-14 22:32:29 +0800

    add DFS

--- 7-Graph/Adjacency-Table.c
+++ 7-Graph/Adjacency-Table.c
@@ -6,6 +6,7 @@
  */
 
 #define maxSize 10
+int visit[maxSize];
 
 // 边表结点
 typedef struct ArcNode {
@@ -25,6 +26,25 @@ typedef struct {
 	VNode adjlist[maxSize];
 	int n, e;
 } AGraph;
+
+// 访问结点
+void Visit(int v) {
+	printf("%d ", v);
+}
+
+// 深度优先搜索
+void DFS(AGraph *G, int v) {
+	ArcNode *p;
+	visit[v] = 1;
+	Visit(v);
+	p = G->adjlist[v].firstarc;
+	if (p != NULL) {
+		if (visit[p->adjvex] == 0) {
+			DFS(G, p->adjvex);
+		}
+		p = p->nextarc;
+	}
+}
 
 int main() {
 	return 0;
